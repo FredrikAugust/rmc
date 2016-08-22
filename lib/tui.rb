5f7b73c6cdae3ffbf94274ae9942832fe390b255
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-08-22 19:40:06 +0200

    added basic loop to exit when someone presses 'q'

--- lib/tui.rb
+++ lib/tui.rb
@@ -1,3 +1,26 @@
 require 'ncurses'
 require './back-end.rb'
 
+# create a screen object
+Ncurses.initscr
+
+begin
+  Ncurses.cbreak()
+  # don't output what you type on the screen
+  Ncurses.noecho()
+
+  # don't show cursor
+  Ncurses.curs_set(0)
+
+  # 113 == 'q' in the ascii table
+  while((ch = Ncurses.getch()) != 113) do
+    case(ch.chr)
+    when 'p'
+    end
+    
+    Ncurses.refresh()
+  end
+ensure
+  Ncurses.curs_set(1)
+  Ncurses.endwin()
+end
